ee535b6b94a1c5755a7c0e424171cc2f7df7bd61
michaelsinanta <michaelchristlambert@gmail.com>
2023-11-29 16:49:32 +0700

    init

--- generator/generator.py
+++ generator/generator.py
@@ -1,24 +1,13 @@
-import random, time
+import math
+import random
+import os
+import time
 
 INF = 9999
-
 random.seed(time.time())
 
-def generate_graph_edge_list(N: int, E: int):
-    adj_matrix = [ ([False for j in range(N+1)]) for i in range(N+1) ]
-    edge_list = []
-    while E > 0:
-        u = random.randint(1, N)
-        v = random.randint(1, N)
-        if not adj_matrix[u][v] and u != v:
-            edge_list.append((u, v))
-            adj_matrix[u][v] = True
-            adj_matrix[v][u] = True
-            E -= 1
-    return edge_list
-
 def generate_graph_matrix(N: int, E: int):
-    adj_matrix = [ ([INF for j in range(N+1)]) for i in range(N+1) ]
+    adj_matrix = [[INF for _ in range(N + 1)] for _ in range(N + 1)]
     while E > 0:
         u = random.randint(1, N)
         v = random.randint(1, N)
@@ -29,25 +18,36 @@ def generate_graph_matrix(N: int, E: int):
             E -= 1
     return adj_matrix
 
-# edge_list = generate_graph_matrix(1024, 10)
+vrtx = [20, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1024]
 
-# output_string = ''
-# for a, b in edge_list:
-#     output_string += f'{a} {b}\n'
+def save_matrices_to_files(adj_matrices, size, category):
+    folder_name = f"algos/{category}/input"
+    os.makedirs(folder_name, exist_ok=True)
 
-# with open('output/out1.txt', 'w') as file:
-#     file.write(output_string)
+    for idx, adj_matrix in enumerate(adj_matrices):
+        output_string = f'{size}\n'
+        for i in range(size):
+            for j in range(size):
+                output_string += str(adj_matrix[i][j])
+                if j < size - 1:
+                    output_string += ' '
+                else:
+                    output_string += '\n'
 
-adj_matrix =  generate_graph_matrix(1024, 10)
+        file_path = os.path.join(folder_name, f'in{idx}.txt')
+        with open(file_path, 'w') as file:
+            file.write(output_string)
 
-output_string = f'{1024}\n'
-for i in range(1024):
-    for j in range(1024):
-        output_string += str(adj_matrix[i][j])
-        if j < 1023:
-            output_string += ' '
-        else:
-            output_string += '\n'
-
-with open('output/out1.txt', 'w') as file:
-    file.write(output_string)
+for category, size in enumerate(vrtx):
+    adj_matrices = [
+        generate_graph_matrix(size, size // 4),
+        generate_graph_matrix(size, size // 2),
+        generate_graph_matrix(size, size),
+        generate_graph_matrix(size, 2 * size),
+        generate_graph_matrix(size, 4 * size),
+        generate_graph_matrix(size, int(math.log2(size) * size)),
+        generate_graph_matrix(size, int(2 * size * math.log2(size))),
+        generate_graph_matrix(size, int(size * size // math.log2(size))),
+        generate_graph_matrix(size, size * (size - 1) // 2)
+    ]
+    save_matrices_to_files(adj_matrices, size, category)
